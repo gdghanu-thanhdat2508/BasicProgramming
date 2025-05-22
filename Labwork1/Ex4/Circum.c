209e63d00d5c098e4a2381e25df193c5ac495128
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-05-22 17:38:09 +0700

    first commit

--- Labwork1/Ex4/Circum.c
+++ Labwork1/Ex4/Circum.c
@@ -2,10 +2,12 @@
 
 #define PI 3.14159
 
-double calculateCircumference(double radius) {
+double calculateCircumference(double radius)
+{
     return 2 * PI * radius;
 }
 
-double calculateArea(double radius) {
+double calculateArea(double radius)
+{
     return PI * radius * radius;
 }
